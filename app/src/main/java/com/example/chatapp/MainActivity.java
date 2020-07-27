a9fa7f51f72f7d372bcf99b63da4dbd52246a46a
anamitra30 <anamitramukherjee123@gmail.com>
2020-07-27 16:40:23 +0530

    Added Login,Registration Screens

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button registerButton = findViewById(R.id.register_btn);
+        Button loginButton = findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login_intent = new Intent(getApplicationContext(), LoginScreen.class);
+                startActivity(login_intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registration_intent = new Intent(getApplicationContext(),
+                        RegistrationScreen.class);
+                startActivity(registration_intent);
+            }
+        });
     }
+
 }
